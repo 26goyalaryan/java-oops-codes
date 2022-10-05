b5b562d91b51f3044541887538b20ef78dad9fd8
aryan <26goyalaryan@gmail.com>
2022-10-06 00:20:43 +0530

    personal check

--- com.javaoops/src/Main.java
+++ com.javaoops/src/Main.java
@@ -3,9 +3,7 @@ public class Main {
         textbox t1= new textbox();
         // var t1 = new textbox();
         // this will initialise t1 on the basis of what is on the right side of =
-        t1.text="box1";
-        System.out.println(t1.text);
-        t1.clear();
+        t1.text="";
         System.out.println(t1.text);
     }
 }
